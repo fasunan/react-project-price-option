03e32a812d704d552807077792d7967d010d872a
fasunan <fasunan@gmail.comm>
2023-09-19 14:55:53 +0600

    For navber all set up is done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import NavBar from "./Components/NavBar/NavBar";
+// import DaisyNav from "./Components/daisyNav/DaisyNav";
+
+function App() {
+  return (
+    <>
+      {/* <DaisyNav></DaisyNav> */}
+      <NavBar></NavBar>
+    </>
+  );
+}
+
+export default App;
